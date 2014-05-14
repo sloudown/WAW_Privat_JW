dda65bee1297140d0c50f07623ab8cb47b90ca07
Johannes Weber <weber.johanes@googlemail.com>
2014-05-14 21:15:03 +0200

    kostenTabelleFuellen() angefangen…
    ...mit javascript, ein problem ist noch vorhanden und code muss
    sauberer gemacht werden

--- WAW_Project_JW/projekt/meilenstein3/javascript/detail.js
+++ WAW_Project_JW/projekt/meilenstein3/javascript/detail.js
@@ -43,57 +43,120 @@ function klick(id) {
 }
 
 /**
- * Funktion um die Tabelle, mit Hilfe von JSON Objekten, mit Details zu fuellen.
+ * Funktion um die Tabelle,fuer die Kosten der einzelnen Messen,
+ * mit Hilfe von JSON Objekten, mit Details zu fuellen.
  */
-function tabelleFuellen(){
+function kostenTabelleFuellen(){
+    /**
+     * Die JSON Objekte der Kosten.
+     */
+
+    var header = [
+        {ueberschrift: "Messe"},
+        {ueberschrift: "Selbstkosten"}
+    ];
+
+    var kostenCebit = {
+        name: "CeBit",
+        kosten: "15"
+    };
+
+    var kostenConhit = {
+        name: "ConhIT",
+        kosten: "50"
+    };
+
+    var kostenWebtechcon = {
+        name: "WebTechCon",
+        kosten: "300"
+    };
+
+    // das div tag mit der id details holen
+    var detailsDiv = document.getElementById('details');
+    // das table tag erstellen
+    var tableTag = document.createElement('table');
+    // dem tableTag die id tabelle geben
+    tableTag.setAttribute('id', 'tabelle');
+    // rowTag erstellen
+    var rowTag = document.createElement('tr');
+
+    //rowTag mit einer foreach schleife fuellen
+    for (i in header){
+        var cellTag = document.createElement('td');
+        var textNode = document.createTextNode(i.ueberschrift); //warum steht in der HTML Datei undefined?
+        cellTag.appendChild(textNode);
+        rowTag.appendChild(cellTag);
+    }
+    tableTag.appendChild(rowTag);
+    detailsDiv.appendChild(tableTag);
 
 }
 
-/**
- * Die JSON Objekte um die Tabelle zu fuellen
- */
+function teilnehmerTabelleFuellen(){
+    /**
+     * Die JSON Objekte der einzelnen Teilnehmer
+     */
 
-var kostenCebit = {
-    name: "CeBit",
-    kosten: "15"
-};
+    var teilnehmercebit1 = {
+        name: "Ent",
+        vorname: "Thilo",
+        studiengang: "IB",
+        email: "t.ent@gmail.de"
+    };
 
-var kostenConhit = {
-    name: "ConhIT",
-    kosten: "50"
-};
+    var teilnehmercebit2 = {
+        name: "Kraus",
+        vorname: "Martina",
+        studiengang: "IB",
+        email: "m.kraus@hs-mannheim.de"
+    };
 
-var kostenWebtechcon = {
-    name: "WebTechCon",
-    kosten: "300"
-};
+    var teilnehmercebit3 = {
+        name: "Smits",
+        vorname: "Thomas",
+        studiengang: "UIB",
+        email: "t.smits@hs-mannheim.de"
+    };
 
-var teilnehmercebit1 = {
-    name: "Ent",
-    vorname: "Thilo",
-    studiengang:  "IB",
-    email: "t.ent@gmail.de"
-};
+    var teilnehmercebit4 = {
+        name: "Groeschel",
+        vorname: "Michael",
+        studiengang: "UIB",
+        email: "m.groeschel@hs-mannheim.de"
+    };
 
-var teilnehmercebit2 = {
-    name: "Kraus",
-    vorname: "Martina",
-    studiengang:  "IB",
-    email: "m.kraus@hs-mannheim.de"
-};
+    var teilnehmerconhit1 = {
+        name: "Schmuecker",
+        vorname: "Paul",
+        studiengang: "IMB",
+        email: "p.schmuecker@hs-mannheim.de"
+    };
 
-var teilnehmercebit3 = {
-    name: "Smits",
-    vorname: "Thomas",
-    studiengang:  "UIB",
-    email: "t.smits@hs-mannheim.de"
-};
+    var teilnehmerconhit2 = {
+        name: "Felsenheimer",
+        vorname: "Jan",
+        studiengang: "IB",
+        email: "j.felsenheimer@diebestebaendderwelt.de"
+    };
 
-var teilnehmercebit4 = {
-    name: "Groeschel",
-    vorname: "Michael",
-    studiengang:  "UIB",
-    email: "m.groeschel@hs-mannheim.de"
-};
+    var teilnehmerwebtechcon1 = {
+        name: "Spies",
+        vorname: "Marcell",
+        studiengang: "IB",
+        email: "m.spies@gmail.de"
+    };
 
+    var teilnehmerwebtechcon2 = {
+        name: "Hoppe",
+        vorname: "Johannes",
+        studiengang: "UIB",
+        email: "j.hoppe@gmx.de"
+    };
 
+    var teilnehmerwebtechcon3 = {
+        name: "Hofmeister",
+        vorname: "Johannes",
+        studiengang: "UIB",
+        email: "cessor@gmail.de"
+    };
+}
