1ca142b15cd489a2787ad3f111a85e1cf6dad0db
Johannes Weber <weber.johanes@googlemail.com>
2014-05-28 20:08:35 +0200

    validierung geändert

--- WAW_Project_JW/projekt/meilenstein3/javascript/index.js
+++ WAW_Project_JW/projekt/meilenstein3/javascript/index.js
@@ -1,67 +1,94 @@
 /**
- * Selbst erstellte Regeln zur Validierung des Formulars
+ * Diese Funktion ueberprueft die einzelnen Felder des Formulars
+ * @param form das formular welches ueberprueft werden soll
+ * @returns {boolean}
  */
 
-$(document).ready(function(){
+function pruefen(form){
 
-    $("#formular").validate({
+    //pruefen auf vollstaendigkeit
+    var fehlertext = "Einige Eingaben sind fehlerhaft! Bitte überprüfen Sie Ihre Eingaben.";
+    var fehler = false;
+    if(form.elements['vorname'].value == ''){
+        fehler = true;
+    }
+    if(form.elements['name'].value == ''){
+        fehler = true;
+    }
+    if(form.elements['matrikelnr'].value == ''){
+        fehler = true;
+    }
+    if(form.elements['email'].value == ''){
+        fehler = true;
+    }
+    if(form.elements['handy'].value == ''){
+        fehler = true;
+    }
+    // Pruefung auf Zahlen von matrikelnummer und der handynummer
+    if(!istZahl(form.elements['matrikelnr'].value)){
+        fehler = true;
+    }
 
-        rules: {
+    if(!istZahl(form.elements['handy'].value)){
+        fehler = true;
+    }
 
-            vorname: "required",
+    //Pruefung auf Buchstaben von Namen und Vornamen
 
-            name: "required",
+    if(istZahl(form.elements['vorname'].value)){
+        fehler = true;
+    }
 
-            email: {
+    if(istZahl(form.elements['name'].value)){
+        fehler = true;
+    }
 
-                required: true,
-                email: true
+    //pruefung der email
+    if(!istEmail(form.elements['matrikelnr'].value)){
+        fehler = true;
+    }
 
-            },
+    //wenn ein feld falsch true zurueckliefert wurde wird ein fehlertext ausgegeben
+    if(fehler){
+        alert(fehlertext);
+        return false;
+    }else {
+        return true;
+    }
+}
 
-            matrikelnr: {
-
-                required: true,
-                digits: true
-            },
-
-            handy: {
-
-                required: true,
-                digits: true
-            }
-
-        },
-
-        messages: {
-
-            vorname: "Bitte Vornamen angeben!",
-
-            name: "Bitte Namen angeben!",
-
-            email: {
-
-                required: "Bitte E-Mail-Adresse eingeben!",
-                email: "E-Mail im Format name@domain.de eingeben!"
-
-            },
-
-            matrikelnr: {
-
-                required: "Bitte Matrikelnummer eingeben!",
-                digits: "Bitte nur Zahlen verwenden"
-
-            },
-
-            handy: {
-
-                required: "Bitte Handynummer eingeben!",
-                digits: "Bitte nur Zahlen eingeben"
-
-            }
-
+/**
+ * Diese Funktion gibt true zurueck wenn die eingabe nur aus zahlen besteht
+ * und false wenn nicht.
+ * @param eingabe
+ * @returns {boolean}
+ */
+function istZahl(eingabe) {
+    var nummer = "" + eingabe;
+    var zahlen = "0123456789";
+    for (var i = 0; i < nummer.length; i++){
+        if (zahlen.indexOf(nummer.charAt(i)) == -1){
+            return false;
         }
+    }
+    return true;
+}
+/**
+ * Diese Funktion prueft ob es sich bei dem uebergebenen wert um eine korrekte email handelt
+ * @param mail
+ */
+function istEmail(mail){
+    var at = mail.indexOf('@');
+    if (at < 1){
+        return false;
+    }else{
+        var punkt = mail.substring(at).indexOf('.');
+        if (punkt < 2){
+            return false;
+        }else {
+            return true;
+        }
+    }
+}
 
-    });
 
-});
